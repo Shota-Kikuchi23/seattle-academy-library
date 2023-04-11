f86cc86c2fb3113e52af668000dc01f522de8456
kikuchishouta <kikuchishouta@localhost>
2023-04-11 19:12:55 +0900

    task2フォーマット修正

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -42,13 +42,11 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-		if(selectedUserInfo != null ) {
+		if (selectedUserInfo != null) {
 			return "redirect:/home";
 		} else {
 			model.addAttribute("errorMessage", "パスワードが一致しません。");
-		    return "createAccount";
+			return "createAccount";
 		}
 	}
 }
-		
-	
